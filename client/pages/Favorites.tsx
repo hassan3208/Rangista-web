1ccb5ff32d8b1eafe546782a017160c2a64c1202
Hassan <itsmywork1019@gmail.com>
2025-10-18 17:25:24 +0500

    update Arrow in favourites

--- client/pages/Favorites.tsx
+++ client/pages/Favorites.tsx
@@ -23,12 +23,18 @@ export default function Favorites() {
               collection={p.collection as "Eid Collection" | "Bakra Eid Specials" | "14 August Independence Collection" | "Birthday Specials"}
               total_reviews={p.total_reviews}
               average_rating={p.average_rating}
+              XS_price={p.XS_price}
               S_price={p.S_price}
               M_price={p.M_price}
               L_price={p.L_price}
+              XL_price={p.XL_price}
+              XXL_price={p.XXL_price}
+              XS_stock={p.XS_stock}
               S_stock={p.S_stock}
               M_stock={p.M_stock}
               L_stock={p.L_stock}
+              XL_stock={p.XL_stock}
+              XXL_stock={p.XXL_stock}
               kids={p.kids}
             />
           ))}
